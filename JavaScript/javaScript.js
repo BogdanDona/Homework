bcd62aeb5458a1032713932f38706d445531a8ce
BogdanDona <bogdandona04@gmail.com>
2024-05-15 22:57:57 +0300

    Create javaScript.js

---
+++ JavaScript/javaScript.js
@@ -0,0 +1,39 @@
+// ex. 1
+//for (let i = 1; i <= 20; i++) console.log(i);
+
+// ex. 2
+//for (let i = 1; i <= 20; i += 2) console.log(i);
+
+// ex. 3
+// let array = [14, 4, 20, 51, 47];
+// let sum = 0;
+// for (let i = 0; i < array.length; i++) sum += array[i];
+// console.log(sum);
+
+// ex. 4
+// let array = [14, 4, 20, 51, 47];
+// let max = array[0];
+// for (let i = 0; i < array.length; i++) if (array[i] >= max) max = array[i];
+// console.log(max);
+
+// ex. 5
+// let array = [14, 4, 20, 51, 47, 4, 4, 14, 51, 47, 47, 47];
+// const count = {};
+// for (let i = 0; i < array.length; i++) {
+//   if (count[array[i]]) count[array[i]]++;
+//   else count[array[i]] = 1;
+// }
+// console.log(count);
+
+// ex. 6
+// for (let i = 0; i < 4; i++) {
+//   for (let j = 0; j < 4; j++)
+//     if (i % 2 == 0) {
+//       if (j % 2 == 0) console.log(0, " ");
+//       else console.log(1, " ");
+//     } else {
+//       if (j % 2 == 0) console.log(1, " ");
+//       else console.log(0, " ");
+//     }
+//   console.log("\n");
+// }
